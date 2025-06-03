e99cb87920cb847208921b1053a6a77eff469ce1
Arimamarieru <overlordhok1@gmail.com>
2025-06-03 15:17:24 +0300

    Update evens.py

--- part4/evens.py
+++ part4/evens.py
@@ -1,4 +1,5 @@
-a, b = map(int, input("Введите два целых числа A и B").split())
+print("Введите два целых числа A и B:")
+a, b = map(int, input().split())
 
-start = a if a % 2 == 0 else a + 1      # первое чётное ≥ A
+start = a if a % 2 == 0 else a + 1
 print(*range(start, b + 1, 2))
