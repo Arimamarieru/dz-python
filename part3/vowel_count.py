b4e3f72747e1145e137688ea5a4fd63162ed2142
Arimamarieru <overlordhok1@gmail.com>
2025-06-03 10:13:54 +0300

    Create vowel_count.py

---
+++ part3/vowel_count.py
@@ -0,0 +1,14 @@
+word = input("Введите слово из маленьких латинских букв: ")
+
+vowels = 'aeiou'
+vowel_counts = {v: word.count(v) for v in vowels}
+total_vowels = sum(vowel_counts.values())
+total_consonants = len(word) - total_vowels
+
+print("Гласных:", total_vowels)
+print("Согласных:", total_consonants)
+
+# Печатаем количество каждой гласной, иначе False
+for v in vowels:
+    count = vowel_counts[v]
+    print(f"{v}: {count if count > 0 else False}")
