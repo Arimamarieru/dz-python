8bb1b62332280bb2d47273c4189456845789fe79
Arimamarieru <overlordhok1@gmail.com>
2025-06-03 15:10:57 +0300

    Update divs.py

--- part4/divs.py
+++ part4/divs.py
@@ -1,8 +1,10 @@
-x = int(input())
+x = int(input("Введите натуральное число X")
+
 cnt = 0
 i = 1
 while i * i <= x:
     if x % i == 0:
         cnt += 1 if i * i == x else 2
     i += 1
-print(cnt)
+
+print("Количество натуральных делителей числа:", cnt)
