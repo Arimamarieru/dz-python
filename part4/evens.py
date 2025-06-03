d8d1104904f602cc4478e80c8f6d872bd8e26356
Arimamarieru <overlordhok1@gmail.com>
2025-06-03 16:04:42 +0300

    Update evens.py

--- part4/evens.py
+++ part4/evens.py
@@ -2,4 +2,7 @@ print("Введите два целых числа A и B:")
 a, b = map(int, input().split())
 
 start = a if a % 2 == 0 else a + 1
-print(*range(start, b + 1, 2))
+even_numbers = list(range(start, b + 1, 2))
+
+print(f"Чётные числа на отрезке от {a} до {b}:")
+print(*even_numbers)
