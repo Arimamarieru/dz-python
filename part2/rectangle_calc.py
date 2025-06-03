451d1c2a80f61d3ba81b5844321f2a1adbbb7464
Arimamarieru <overlordhok1@gmail.com>
2025-06-03 09:01:13 +0300

    Create rectangle_calc.py

---
+++ part2/rectangle_calc.py
@@ -0,0 +1,9 @@
+# Задание 1:
+a = float(input("Введите длину стороны A: "))
+b = float(input("Введите длину стороны B: "))
+
+area = a * b
+perimeter = 2 * (a + b)
+
+print(f"Площадь: {area}")
+print(f"Периметр: {perimeter}")
