0d8bbd7191617d51a412390c551ff6df496f60c1
Arimamarieru <overlordhok1@gmail.com>
2025-06-03 14:44:40 +0300

    Update divs.py

--- part4/divs.py
+++ part4/divs.py
@@ -1,12 +1,8 @@
-import math
-
-x = int(input("Введите натуральное число X (≤ 2 000 000 000): "))
-
+x = int(input())
 cnt = 0
 i = 1
 while i * i <= x:
     if x % i == 0:
         cnt += 1 if i * i == x else 2
     i += 1
-
 print(cnt)
