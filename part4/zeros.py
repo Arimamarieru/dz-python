dd6a4272132caf33aaa943e638eb32761aacb9fe
Arimamarieru <overlordhok1@gmail.com>
2025-06-03 15:14:21 +0300

    Update zeros.py

--- part4/zeros.py
+++ part4/zeros.py
@@ -1,6 +1,8 @@
-n = int(input())
+n = int(input("Сколько чисел вы собираетесь ввести? "))
 zeros = 0
-for _ in range(n):
-    if int(input()) == 0:
+
+for i in range(1, n + 1):
+    if int(input(f"Введите число №{i}: ")) == 0:
         zeros += 1
-print(zeros)
+
+print("Количество нулей:", zeros)
