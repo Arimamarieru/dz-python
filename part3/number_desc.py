8d91b87ecc95981a33af749828b31a58c8689c41
Arimamarieru <overlordhok1@gmail.com>
2025-06-03 09:56:26 +0300

    Create number_desc.py

---
+++ part3/number_desc.py
@@ -0,0 +1,12 @@
+number = int(input("Введите целое число: "))
+
+if number == 0:
+    print("нулевое число")
+elif number > 0 and number % 2 == 0:
+    print("положительное четное число")
+elif number > 0 and number % 2 != 0:
+    print("положительное нечетное число")
+elif number < 0 and number % 2 == 0:
+    print("отрицательное четное число")
+else:
+    print("число не является четным")
