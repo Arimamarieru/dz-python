d4cae9f2b9fcfb22a2162dcaf70f12f0e37a8609
Arimamarieru <overlordhok1@gmail.com>
2025-06-09 08:23:56 +0300

    Update game.py

--- game/core/game.py
+++ game/core/game.py
@@ -1,1 +1,22 @@
+# core/game.py
+from time import sleep
+
+from .config import GameConfig
+from .field import GameMap
+from .helicopter import Helicopter
+
+class Game:
+    """Простейший игровой цикл — только выводит число тиков."""
+
+    def __init__(self, cfg: GameConfig):
+        self.cfg = cfg
+        self.map = GameMap(cfg)
+        self.heli = Helicopter(self.map)
+        self.tick_counter = 0
+
+    def run(self):
+        while True:
+            self.tick_counter += 1
+            print(f"Tick #{self.tick_counter}")
+            sleep(self.cfg.tick_delay)
 
